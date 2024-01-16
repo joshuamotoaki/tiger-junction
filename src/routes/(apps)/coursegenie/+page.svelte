1a604643a2204f4b398d4495e9b3ac3e41f3d17c
joshuamotoaki <dachshunduniverse@gmail.com>
2024-01-16 18:48:51 -0500

    Boilerplate reqtree

--- src/routes/(apps)/coursegenie/+page.svelte
+++ src/routes/(apps)/coursegenie/+page.svelte
@@ -1,5 +1,8 @@
 <script lang='ts'>
-import { EMAIL_LIST_FORM_LINK } from "$lib/constants";
+import Kanban from "./Kanban.svelte";
+import Requirements from "./Requirements.svelte";
+import Search from "./Search.svelte";
+
 
 </script>
 
@@ -7,28 +10,15 @@ import { EMAIL_LIST_FORM_LINK } from "$lib/constants";
     <title>TigerJunction | CourseGenie</title>
 </svelte:head>
 
-<div class="w-full h-[80vh] flex flex-col justify-center
-items-center gradient-text text-5xl font-bold gap-2 text-center">
-    <span>
-        CourseGenie Coming Soon
-    </span>
-    <a href={EMAIL_LIST_FORM_LINK} target="_blank">
-        <button class="btn-cool text-xl">
-            Get Notified About Updates
-        </button>
-    </a>
-</div>
-
-<style lang='postcss'>
-.gradient-text {
-    @apply bg-gradient-to-r from-deepblue-light to-deepblue-dark;
-    -webkit-background-clip: text; /* For Safari */
-    background-clip: text;
-    color: transparent;
-}
-
-.btn-cool {
-    @apply bg-gradient-to-r from-deepblue-light to-deepblue-dark
-    text-zinc-100 rounded-full px-4 py-2;
-}
-</style>
+<main class="flex flex-col flex-1 w-full max-w-[1500px] mx-auto
+dark:bg-black max-h-screen overflow-clip">
+    <div id="search">
+        <Search />
+    </div>
+    <div id="kanban">
+        <Kanban />
+    </div>
+    <div id="requirements">
+        <Requirements />
+    </div>
+</main>
