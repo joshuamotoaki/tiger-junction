ec2d1b9c5dd024092ca6c1f5447db38a5de550b0
joshuamotoaki <dachshunduniverse@gmail.com>
2024-01-16 18:57:00 -0500

    Start ReqTree

--- src/routes/(apps)/reqtree/+page.svelte
+++ src/routes/(apps)/reqtree/+page.svelte
@@ -1,34 +1,22 @@
 <script lang='ts'>
-import { EMAIL_LIST_FORM_LINK } from "$lib/constants";
+    import Board from "./Board.svelte";
 
+    
 </script>
 
 <svelte:head>
-    <title>TigerJunction | ReqTree</title>
+    <title>TigerJunction | CourseGenie</title>
 </svelte:head>
 
-<div class="w-full h-[80vh] flex flex-col justify-center
-items-center gradient-text text-5xl font-bold gap-2 text-center">
-    <span>
-        ReqTree Coming Soon
-    </span>
-    <a href={EMAIL_LIST_FORM_LINK} target="_blank">
-        <button class="btn-cool text-xl">
-            Get Notified About Updates
-        </button>
-    </a>
-</div>
-
-<style lang='postcss'>
-.gradient-text {
-    @apply bg-gradient-to-r from-deepblue-light to-deepblue-dark;
-    -webkit-background-clip: text; /* For Safari */
-    background-clip: text;
-    color: transparent;
-}
-
-.btn-cool {
-    @apply bg-gradient-to-r from-deepblue-light to-deepblue-dark
-    text-zinc-100 rounded-full px-4 py-2;
-}
-</style>
+<main class="flex flex-col flex-1 w-full max-w-[1500px] mx-auto
+dark:bg-black max-h-screen overflow-clip">
+    <div id="search">
+        
+    </div>
+    <div id="board">
+        <Board />
+    </div>
+    <div id="requirements">
+        
+    </div>
+</main>
